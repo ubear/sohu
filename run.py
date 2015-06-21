9da9507ccfe502c4130da6562fda3966554abc1a
chyoo <chyoo1991@gmail.com>
2015-06-21 09:01:36 +0800

    Remove .idea

--- run.py
+++ run.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # coding=utf8
 
-# 执行初始化脚本
-# 开始定时运行
-# 本次爬取日志设置,日志文件名要带时间的
-# 多线程爬取
-# 结束/数据统计/比如运行了多长时间
-
 import os
 
 from apscheduler.schedulers.blocking import BlockingScheduler
